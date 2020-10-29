9308b6da2bef56023afcaab1a56b03a8dbbd5d79
Ken Leung <kenleung5e28@gmail.com>
2020-10-29 17:20:20 +0800

    update division for decimals

--- src/arithmetic.ts
+++ src/arithmetic.ts
@@ -1,35 +1,58 @@
-export const division = (dividend: string, divisor: string): string => {
-  const a = parseInt(dividend);
-  const b = parseInt(divisor);
-  if (!Number.isInteger(a) || a <= 0 || a > 99999) {
-    throw new Error('"a" must be an integer between 1 and 99999');
+function normalize(num: string, zeroCount: number): [number, number] {
+  const dp = (num.split('.'))[1]?.length ?? 0;
+  return [parseInt(num.split('.').join('') + '0'.repeat(zeroCount)), dp];
+}
+
+function insertCol(sq: string[][], symbol: string, colIndex: number): void {
+  for (let i = 0; i < sq.length; i++) {
+    sq[i].splice(colIndex, 0, symbol);
   }
-  if (!Number.isInteger(b) || b <= 0 || b > 999) {
-    throw new Error('"b" must be an integer between 1 and 999');
+}
+
+export function division(dividend: string, divisor: string): string {
+  if (isNaN(parseFloat(dividend))) {
+    throw new Error('"dividend" must be a number');
+  }
+  if (isNaN(parseFloat(divisor))) {
+    throw new Error('"divisor" must be a number');
+  }
+  const dividendDecPartLen = (dividend.split('.'))[1]?.length ?? 0;
+  const divisorDecPartLen = (divisor.split('.'))[1]?.length ?? 0;
+  const a0Pad = dividendDecPartLen > divisorDecPartLen ? 0 : divisorDecPartLen - dividendDecPartLen;
+  const b0Pad = divisorDecPartLen > dividendDecPartLen ? 0 : dividendDecPartLen - divisorDecPartLen;
+  const [a, aDp] = normalize(dividend, a0Pad);
+  const [b, bDp] = normalize(divisor, b0Pad);
+  const normalizedDividend = a.toString();
+  const normalizedDivisor = b.toString();
+  // console.log({ a, a0Pad, aDp, b, b0Pad, bDp });
+  if (!Number.isInteger(a) || a <= 0) {
+    throw new Error('"dividend" must be a non-negative integer');
+  }
+  if (!Number.isInteger(b) || b <= 0) {
+    throw new Error('"divisor" must be a non-negative integer');
   }
   if (a < b) {
-    throw new Error('"a" must be at least as large as "b"');
+    throw new Error('"dividend" must be at least as large as "divisor"');
   }
-  //console.log({ a, b });
   const quot = parseInt((a / b).toString());
   const rem = a % b;
-  const an = dividend.length;
-  const bn = divisor.length;
+  const an = normalizedDividend.length;
+  const bn = normalizedDivisor.length;
   const qx = quot.toString();
   const qn = qx.length;
   const prefix = '|'.padStart(bn + 1);
   const prefixL = '('.padStart(bn + 1);
-  const put = (num: number, space: number, underline = false) => ((underline ? prefixL: prefix) + (num.toString() + ' '.repeat(space)).padStart(an)).split('');
+  const put = (num: number, space: number, underline = false) => ((underline ? prefixL : prefix) + (num.toString() + ' '.repeat(space)).padStart(an)).split('');
   let sq: string[][] = [[], []];
   sq[0] = (prefix + qx.padStart(an)).split('');
-  sq[1] = (divisor + ')' + dividend).split('')
+  sq[1] = (normalizedDivisor + ')' + normalizedDividend).split('');
   let prod = b * parseInt(qx[0]);
-  let prev = parseInt(dividend.substring(0, an - qn + 1));
+  let prev = parseInt(normalizedDividend.substring(0, an - qn + 1));
   sq.push(put(prod, qn - 1, true));
   for (let i = 1; i < qn; i++) {
-    //console.log({ prev, prod });
+    // console.log({ prev, prod });
     const qi = parseInt(qx[i]);
-    const ai = parseInt(dividend[an - qn + i]);
+    const ai = parseInt(normalizedDividend[an - qn + i]);
     prev = 10 * (prev - prod) + ai;
     prod = b * qi;
     sq.push(put(prev, qn - 1 - i));
@@ -38,18 +61,44 @@ export const division = (dividend: string, divisor: string): string => {
   if (rem !== 0) {
     sq.push(put(rem, 0));
   }
+  // console.log('Before');
+  // console.table(sq);
+  const bPos = bn - bDp - b0Pad;
+  if (bPos > 0) {
+    insertCol(sq, '.', bPos);
+  } else {
+    for (let i = bPos; i < 0; i++) {
+      insertCol(sq, 'z', 0);
+    }
+    insertCol(sq, '.', 0);
+    insertCol(sq, 'z', 0);
+  }
+  const aPos = 1 + sq[1].indexOf(')') + an - aDp - a0Pad;
+  if (aPos > 1 + sq[1].indexOf(')')) {
+    insertCol(sq, '.', aPos);
+  } else {
+    for (let i = bPos; i < 0; i++) {
+      insertCol(sq, 'z', 0);
+    }
+    insertCol(sq, '.', 0);
+    insertCol(sq, 'z', 0);
+  }
+  // console.log('After');
+  // console.table(sq);
+  const separatorPos = sq[1].indexOf(')');
+  // console.log({ separatorPos });
   return sq.map((row, i) => {
-    const type = row[bn];
+    const type = row[separatorPos];
     let s = '';
     let digitPos = -1;
     if (type === '(') {
-      let j = bn + 1;
-      while (sq[i - 1][j] === ' ') {
+      let j = separatorPos + 1;
+      while (sq[i - 1][j] === ' ' || sq[i - 1][j] === 'z' || sq[i - 1][j] === '.') {
         j += 1;
       }
       digitPos = j;
     }
-    for (const [j ,c] of row.entries()) {
+    for (const [j, c] of row.entries()) {
       switch (c) {
         case '|':
         case '(':
@@ -57,6 +106,12 @@ export const division = (dividend: string, divisor: string): string => {
           break;
         case ')':
           s += ')\\overline{';
+          break;
+        case '.':
+          s += i === 1 ? '.' : '\\phantom{.}';
+          break;
+        case 'z':
+          s += i === 1 ? '0' : '\\phantom{0}';
           break;
         case ' ':
         default:
@@ -73,7 +128,7 @@ export const division = (dividend: string, divisor: string): string => {
   }).join('\\\\\n');
 }
 
-export const multiplication = (op1: string, op2: string): string => {
+export function multiplication(op1: string, op2: string): string {
   const a = parseInt(op1);
   const b = parseInt(op2);
   if (!Number.isInteger(a) || b < 0) {
@@ -121,6 +176,6 @@ export const multiplication = (op1: string, op2: string): string => {
     }
     return s;
   }).join('\\\\\n');
-};
+}
 
 export default { division, multiplication };
