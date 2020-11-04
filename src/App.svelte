a3413136936cdfc4f4595f9382a52f2a2db03d3f
Ken Leung <kenleung5e28@gmail.com>
2020-11-05 00:06:58 +0800

    improve UI

--- src/App.svelte
+++ src/App.svelte
@@ -23,8 +23,8 @@ function process() {
 		if (type === 'mul') {
 			expr = multiplication(op1, op2);
 		} else {
-			if (dp > 10){
-				throw new Error('"dp" must be at most 10');
+			if (dp > 5){
+				throw new Error('"dp" must be at most 5');
 			}
 			expr = division(op1, op2, dp);
 		}
@@ -37,14 +37,14 @@ function process() {
 
 function handleOp1(e: Event) {
 	let target = e.target as HTMLInputElement;
-	target.value = target.value.replace(/[^0-9.]/g, '');
+	target.value = target.value.replace(/[^0-9.]/g, '').substr(0, 8);
 	op1 = target.value;
 	process();
 }
 
 function handleOp2(e: Event) {
 	let target = e.target as HTMLInputElement;
-	target.value = target.value.replace(/[^0-9.]/g, '');
+	target.value = target.value.replace(/[^0-9.]/g, '').substr(0, 8);
 	op2 = target.value;
 	process();
 }
@@ -66,43 +66,22 @@ function handleDp(e: Event) {
 
 <style>
 	main {
-		display: flex;
-		height: 100%;
+		margin: 0;
 		background-color: ghostwhite;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-
 	.panel {
+		box-sizing: border-box;
 		background-color: white;
 		padding: 2rem;
+		margin: 1.5rem;
 		border: solid 1px gainsboro;
 		border-radius: 0.25rem;
-		margin: 1.5rem;
-	}
-
-	.left-panel {
-		flex-basis: 25%;
-	}
-
-	.right-panel {
-		flex-basis: 75%;
-	}
-
-	.demo-area {
-		display: flex;
-	}
-
-	.setting-subpanel {
-		flex-basis: 50%;
+		max-width: 750px;
+		min-height: calc(100vh - 3rem);
 	}
 
 	.display-subpanel {
-		flex-basis: 50%;
 		-webkit-user-select: none;  /* Chrome all / Safari all */
   	-moz-user-select: none;     /* Firefox all */
   	-ms-user-select: none;      /* IE 10+ */
@@ -117,36 +96,26 @@ function handleDp(e: Event) {
 </style>
 
 <main>
-	<div class="panel left-panel">
-		<div>
-			<form>
-				<label class="type-option"><input type="radio" id="mul" name="type" value="mul" on:input={handleTypeOption}> Multiplication</label>
-				<label class="type-option"><input type="radio" id="div" name="type" value="div" checked on:input={handleTypeOption}> Division</label>
-			</form> 
-		</div>
-	</div>
-	<div class="panel right-panel">
+	<div class="panel">
 		<h2>{titles[type]}</h2>
-		<div class="demo-area">
-			<div class="setting-subpanel">
-				<form>
-					<label for="op1">{type === 'div' ? 'Dividend' : 'First number'}</label>
-					<br>
-					<input type="text" name="op1" on:input={handleOp1} value={op1}>
-					<label for="op2">{type === 'div' ? 'Divisor' : 'Second number'}</label>
-					<br>
-					<input type="text" name="op2" on:input={handleOp2} value={op2}>
-					{#if type === 'div'}
-						<label for="dp">Decimal places</label>
-						<br>
-						<input type="text" name="dp" on:input={handleDp} value={dp}>
-					{/if}
-				</form>
-				<div class="error">{error}</div>
-			</div>
-			<div class="display-subpanel">
-				{@html processMath('$' + expr + '$')}
-			</div>
+		<form>
+			<label class="type-option"><input type="radio" id="mul" name="type" value="mul" on:input={handleTypeOption}> Multiplication</label>
+			<label class="type-option"><input type="radio" id="div" name="type" value="div" checked on:input={handleTypeOption}> Division</label>
+			<label for="op1">{type === 'div' ? 'Dividend' : 'First number'}</label>
+			<br>
+			<input type="text" name="op1" on:input={handleOp1} value={op1}>
+			<label for="op2">{type === 'div' ? 'Divisor' : 'Second number'}</label>
+			<br>
+			<input type="text" name="op2" on:input={handleOp2} value={op2}>
+			{#if type === 'div'}
+				<label for="dp">Decimal places</label>
+				<br>
+				<input type="text" name="dp" on:input={handleDp} value={dp}>
+			{/if}
+		</form>
+		<div class="error">{error}</div>
+		<div class="display-subpanel">
+			{@html processMath('$' + expr + '$')}
 		</div>
 	</div>
 </main>
